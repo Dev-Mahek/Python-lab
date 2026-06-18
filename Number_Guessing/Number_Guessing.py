f12cd5e33e0bd3c1359b864abcf2868b345b13c5
Mahek Roy <dev-mahek@zohomail.in>
2026-06-18 22:46:33 +0530

    Implement number guessing game
    Added a number guessing game that prompts the user to guess a randomly generated number between 1 and 10, providing feedback on each guess.

--- Number_Guessing/Number_Guessing.py
+++ Number_Guessing/Number_Guessing.py
@@ -1,1 +1,20 @@
+import random
 
+secret_number = random.randint(1, 10)
+attempts = 0
+
+print("🎯 Welcome to Guess the Number!")
+print("I'm thinking of a number between 1 and 10")
+print("Can you guess it?\n")
+
+while True:
+    guess = int(input("Enter your guess: "))
+    attempts += 1
+
+    if guess < secret_number:
+        print("📉 Too low! Try again.\n")
+    elif guess > secret_number:
+        print("📈 Too high! Try again.\n")
+    else:
+        print(f"🎉 Congratulations! You got it right in {attempts} tries!")
+        break
